7308bc9a8b72a94fb05726dd2f8049b659f1153f
evgen <mzena002@gmail.com>
2023-02-14 21:14:13 +0300

    Сложение рандомных чисел и деление

---
+++ example003/Program.cs
@@ -0,0 +1,9 @@
+﻿int num1 = new Random().Next(1,10);
+Console.WriteLine(num1);
+int num2 = new Random().Next(1,10);
+Console.WriteLine(num2);
+Console.WriteLine(num1 + num2);
+
+double num3 = 12;
+double num4 = 5;
+Console.WriteLine(num3 / num4);
