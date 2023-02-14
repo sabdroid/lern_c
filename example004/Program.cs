c49e720f5a52ae8edb8b68734516f6c2a2c2f7e6
evgen <mzena002@gmail.com>
2023-02-15 00:25:33 +0300

    Помучался но сделал

---
+++ example004/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string? username = Console.ReadLine();
+
+if (username == "Женя")
+  {
+    Console.WriteLine("Добро пожаловать!");
+  }
+else
+  {
+    Console.Write("Впервые слышу о тебе," + (username));
+  }
