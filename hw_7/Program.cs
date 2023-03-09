a6c574365a1980e182d7e99acfd7c0526e469fda
Evgeny <mzena002@gmail.com>
2023-03-09 21:38:57 +0300

    Выполнил дз 7

--- hw_7/Program.cs
+++ hw_7/Program.cs
@@ -41,18 +41,17 @@ PrintArray(arr);
 
 /*Задача 50. Напишите программу, которая на вход принимает индексы элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-
-1,1 -> 9
-1,7 -> элемента с данными индексами в массиве нет
 */
 
+// Немного доделал эту задачу, пользователь может вывести эллемент массива который ему нужен или выйдет сообщение что элемента нет если выйдет за пределы массива. Если важно именно решение как в задаче то везде заменить h на m и k на n. И стереть ввод h и k.
+
+/*
 Console.Write("Введите число строк m -> ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите число столбцов n -> ");
 int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
 
 void PrintArray(int[,] arr)
 {
@@ -65,56 +64,84 @@ void PrintArray(int[,] arr)
         Console.WriteLine();
     }
 }
-Console.Write("Введите строку для проверки h -> ");
-int h = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите столбец для проверки k -> ");
-int k = Convert.ToInt32(Console.ReadLine());
+
 
 void FillArray(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
     {
-        for (int j = 1; j < arr.GetLength(1); j++)
+        for (int j = 0; j < arr.GetLength(1); j++)
         {
-            arr[i, j] = new Random().Next(0, 11);
-
-            if (arr[i, j] == arr[h, k])
-            {
-                Console.Write($"Элемент с этим индексов -> {arr[h, k]}");
-            }
-            else if (arr[i, j] != arr[h, k])
-            {
-                Console.WriteLine("эллемента нет");
-            }
+            arr[i, j] = Convert.ToInt32(new Random().Next(0, 11)) ;
         }
     }
 }
 
-int[,] arr = new int[m, n];
+Console.Write("Введите строку для проверки h -> ");
+int h = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите столбец для проверки k -> ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+if (h > arr.GetLength(0) || k > arr.GetLength(1))
+{
+    Console.WriteLine("Такого нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {h} строки и {k} столбца -> {arr[n-1,m-1]}");
+}
 
 FillArray(arr);
 PrintArray(arr);
-
-
-
-
-
+*/
 
 /*Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 Например, задан массив:
-1 4 7 2
-5 9 2 3
-8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 
-/*Дополнительная задача (задача со звёздочкой): Задайте двумерный массив из целых чисел. Определите, есть столбец в массиве, сумма которого, больше суммы элементов расположенных в четырех "углах" двумерного массива.
-Например, задан массив:
-4 4 7 5
-4 3 5 3
-8 1 6 8 -> нет
-*/
 
-/*
-Дополнительная задача 2 (задача со звёздочкой): Вывести первые n строк треугольника Паскаля. Реализовать вывод в виде равнобедренного треугольника.
-*/
+Console.Write("Введите число строк m -> ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов n -> ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m, n];
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = Convert.ToInt32(new Random().Next(0, 11));
+        }
+    }
+}
+
+FillArray(arr);
+
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        avarage = (avarage + arr[i, j]);
+    }
+    avarage = avarage / n;
+    Console.Write(avarage + "; ");
+}
+
+Console.WriteLine();
+PrintArray(arr);
